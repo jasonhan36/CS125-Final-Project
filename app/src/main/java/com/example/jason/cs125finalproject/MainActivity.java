9bf17910276502a133b084d71d137b7b37865016
jason <jasonh3@illinois.edu>
2018-12-03 17:47:19 -0600

    got buttons to work

--- app/src/main/java/com/example/jason/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/jason/cs125finalproject/MainActivity.java
@@ -1,38 +1,58 @@
 package com.example.jason.cs125finalproject;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.content.Intent;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "Me and Raymond need help";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    final Button button = findViewById(R.id.button);
-    button.setOnClickListener(new View.OnClickListener() {
-                                  public void onClick(View v) {
-                                      // Code here executes on main thread after user presses button
-                                      TextView myText = findViewById(R.id.textView3);
-                                      myText.setText("messageegegegeg");
-                                  }});
+        final Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Log.i("MyApp", "This is a messsage log");
+                Toast.makeText(getApplicationContext(), "You have added an ingredient!", Toast.LENGTH_SHORT)
+                        .show();
+                //TextView myText = findViewById(R.id.textView3);
+                //myText.setText("Raymond's Message");
+            }
+        });
+        final Button removeButton = findViewById(R.id.removeButton);
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Log.i("MyApp", "This is a messsage log");
+                Toast.makeText(getApplicationContext(), "You have removed an ingredient!", Toast.LENGTH_SHORT)
+                        .show();
+                //TextView myText = findViewById(R.id.textView3);
+                //myText.setText("Raymond's Message");
+            }
+        });
+        final Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Log.i("MyApp", "This is a messsage log");
+                Toast.makeText(getApplicationContext(), "You have saved!", Toast.LENGTH_SHORT)
+                        .show();
+                //TextView myText = findViewById(R.id.textView3);
+                //myText.setText("Raymond's Message");
+            }
+        });
 
-//    public void sendMessage(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
 //    }
-};}
+    }
+
+    ;
+}
