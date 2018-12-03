5d7e0977ed8b894f1607e0aea27fd77de597f14c
Raymond Chao <raymond.chao16@gmail.com>
2018-12-03 17:19:25 -0600

    working buttons

--- app/src/main/java/com/example/jason/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/jason/cs125finalproject/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.jason.cs125finalproject;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -15,13 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-}
+    final Button button = findViewById(R.id.button);
+    button.setOnClickListener(new View.OnClickListener() {
+                                  public void onClick(View v) {
+                                      // Code here executes on main thread after user presses button
+                                      TextView myText = findViewById(R.id.textView3);
+                                      myText.setText("messageegegegeg");
+                                  }});
+
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
+};}
