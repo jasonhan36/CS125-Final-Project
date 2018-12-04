2f8f6ad866f3ddaf3f1ff009a255be27928ef591
Raymond Chao <raymond.chao16@gmail.com>
2018-12-03 22:49:57 -0600

    web api somewhat works

--- app/src/main/java/com/example/jason/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/jason/cs125finalproject/MainActivity.java
@@ -8,10 +8,23 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "Me and Raymond need help";
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +35,49 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                Log.i("MyApp", "This is a messsage log");
-                Toast.makeText(getApplicationContext(), "You have added an ingredient!", Toast.LENGTH_SHORT)
-                        .show();
-                //TextView myText = findViewById(R.id.textView3);
-                //myText.setText("Raymond's Message");
+//                Log.i("MyApp", "This is a messsage log");
+//                Toast.makeText(getApplicationContext(), "You have added an ingredient!", Toast.LENGTH_SHORT)
+//                        .show();
+                TextView myText = findViewById(R.id.textView3);
+
+                JSONObject jsonBody = new JSONObject();
+                try {
+                    jsonBody.put("query", "kashi");
+                } catch(JSONException e) {
+                    Log.e("MYAPP", "unexpected JSON exception", e);
+                }
+
+
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        //the end point
+                        "https://api.nutritionix.com/v1_1/search",
+                        jsonBody
+                        ,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(final JSONObject response) {
+                                Log.d(TAG, response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(final VolleyError error) {
+                        Log.w(TAG, error.toString());
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        // any app keys and fields that need to be filled
+                        params.put("x-id-key", "623a35a9");
+                        params.put("x-app-key", "59447ffa6f5b0a827dfa2ecca0fc3afc");
+                        params.put("x-remote-user-id", "0");
+                        Log.d(TAG, params.toString());
+                        return params;
+                    }
+                };
+                myText.setText(jsonObjectRequest.toString());
             }
         });
         final Button removeButton = findViewById(R.id.removeButton);
